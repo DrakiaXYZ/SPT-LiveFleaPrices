540ea50d658e580e9d84141a55be48c6478ccfec
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-10-25 22:31:08 -0700

    Initial commit
    General template, mostly unchanged from Chomp example

---
+++ src/mod.ts
@@ -0,0 +1,19 @@
+import { DependencyContainer } from "tsyringe";
+
+import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
+import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+
+class Mod implements IPostDBLoadMod
+{
+    public postDBLoad(container: DependencyContainer): void 
+    {
+        // get database from server
+        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+
+        // Get all the in-memory json found in /assets/database
+        const tables = databaseServer.getTables();
+        
+    }
+}
+
+module.exports = { mod: new Mod() }
